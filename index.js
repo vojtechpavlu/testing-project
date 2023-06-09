b427214ef99e612f49d990104f7de80a51672925
vojtechpavlu <vojtech.pavlu.dev@gmail.com>
2023-06-09 23:28:42 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const port = 8080
 
 app.get("/version", (req, res) => {
-    const version = parseInt(process.env.version)
+    const version = process.env.version;
     res.status(200).send({ version });
 });
 
